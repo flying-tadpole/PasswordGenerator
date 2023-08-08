d834f33ca3fcf8ed9f1970096805f50889228d6c
flyingtadpole <archer.curry@northwestern.edu>
2023-08-08 09:53:34 -0500

    add user choice verification

--- assets/script.js
+++ assets/script.js
@@ -11,27 +11,32 @@ var specialOptions = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '~']
 function generatePassword() {
   // user prompts for length and characters
   // accepts either uppercase or lowercase answers
+  // if user selects length outside parameters, returns error message
   var pickLength = window.prompt("Enter number between 8 and 128.")
-  var includeLower = window.prompt("Include lowercase character? Y/N")
-  var includeUpper = window.prompt("Include uppercase character? Y/N")
-  var includeNumber = window.prompt("Include number? Y/N")
-  var includeSpecial = window.prompt("Include special character? Y/N")
-
+  if (pickLength > 7 && pickLength < 129) {
+    var includeLower = window.prompt("Include lowercase character? Y/N")
+    var includeUpper = window.prompt("Include uppercase character? Y/N")
+    var includeNumber = window.prompt("Include number? Y/N")
+    var includeSpecial = window.prompt("Include special character? Y/N")
+  } else {
+    return window.alert("You must pick a length between 8 and 128.")
+  }
+  
   // creates pool of characters to pick from 
   // includes console logs for troubleshooting
   // each array is appended as needed if picked by user
   var choicesPool = []
 
-  if (includeLower == 'Y' || 'y') {
+  if (includeLower === 'Y' || includeLower === 'y') {
     choicesPool.push(...lowerOptions)
   }
-  if (includeUpper == 'Y' || 'y') {
+  if (includeUpper === 'Y' || includeUpper === 'y') {
     choicesPool.push(...upperOptions)
   }
-  if (includeNumber == 'Y' || 'y') {
+  if (includeNumber === 'Y' || includeNumber === 'y') {
     choicesPool.push(...numOptions)
   }
-  if (includeSpecial == 'Y' || 'y') {
+  if (includeSpecial === 'Y' || includeSpecial === 'y') {
     choicesPool.push(...specialOptions)
   }
   console.log(choicesPool)
@@ -39,16 +44,21 @@ function generatePassword() {
   // sets variable for password prior to random generation
   var createdPassword = ""
 
+  // checks to make sure character sets have been selected, if no character sets selected, gives error
   // loop that picks random number from total length of choices array, looks up number at index, and adds index item to password being created
-  for (let i = 0; i < pickLength; i++) {
-    numPick = Math.floor(Math.random() * choicesPool.length);
-    console.log(`this is numPick  ${numPick}`)
-    indexFind = choicesPool.at(numPick)
-    console.log(`this is indexFind ${indexFind}`)
-    createdPassword += indexFind
-    console.log(`this is createdPassword so far ${createdPassword}`)
+  if (choicesPool.length !== 0 && pickLength > 7) {
+    for (let i = 0; i < pickLength; i++) {
+      numPick = Math.floor(Math.random() * choicesPool.length);
+      console.log(`this is numPick  ${numPick}`)
+      indexFind = choicesPool.at(numPick)
+      console.log(`this is indexFind ${indexFind}`)
+      createdPassword += indexFind
+      console.log(`this is createdPassword so far ${createdPassword}`)
+    }
+  } else {
+      return window.alert("You must select at least one type of character to include.")
   }
-
+  
   // returns created password as output for page to use
   return createdPassword
 }
